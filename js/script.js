c210cc7c55cae4dbefb306e4b5ce93abac0ddd41
Alex-N40 <alex_n40@proton.me>
2023-11-08 22:22:30 -0600

    Finished project, have yet to fix background image issue

--- js/script.js
+++ js/script.js
@@ -7,6 +7,23 @@ var subDuration = 1; /* This is the default value for the Duration drop-down lis
 
 subTypeElement.addEventListener("change", function(e) {
     subType = e.target.value; /* The target is the option element in HTML. */
-    console.log(subType);
-})
+    //console.log(subType);
+    updateSubscriptionDiv();
+});
 
+subDurationElement.addEventListener("change", function(e) {
+    subDuration = Number(e.target.value);
+    //console.log(subDuration);
+    updateSubscriptionDiv();
+});
+
+var updateSubscriptionDiv = function() {
+    var monthlyCost = 5; /*This is the monthly cost for a basic subscription, which is selected by default. */
+    if(subType === "standard") {
+        monthlyCost = 7;
+    } else if(subType === "premium") {
+        monthlyCost = 10;
+    }
+    var total = subDuration * monthlyCost;
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
